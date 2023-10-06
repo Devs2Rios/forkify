406e4a61006d90aa253ce354d5a0f9ed77a4d0a0
eduairet <hola@eduairet.com>
2023-10-05 20:03:33 -0600

    Persistent Bookmarks Refactor

--- src/js/model/bookmark.js
+++ src/js/model/bookmark.js
@@ -1,5 +1,9 @@
 import { state } from './state';
 
+const persistentBookmarks = () => {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+};
+
 export const loadBookmarks = () => {
   const storage = localStorage.getItem('bookmarks');
   if (storage) state.bookmarks = JSON.parse(storage);
@@ -8,12 +12,12 @@ export const loadBookmarks = () => {
 export const addBookmark = recipe => {
   state.bookmarks.push(recipe);
   if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
-  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+  persistentBookmarks();
 };
 
 export const deleteBookmark = id => {
   const index = state.bookmarks.findIndex(el => el.id === id);
   state.bookmarks.splice(index, 1);
   if (id === state.recipe.id) state.recipe.bookmarked = false;
-  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+  persistentBookmarks();
 };
